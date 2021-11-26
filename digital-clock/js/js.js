279a15c4215d53b4c90787067977877ba87e7278
Hilton Tavares <hilton.tavares.cs00@gmail.com>
2021-11-26 15:05:46 -0100

    update

--- digital-clock/js/js.js
+++ digital-clock/js/js.js
@@ -1,18 +1,12 @@
 
 setInterval(()=>{
-    const time = document.querySelector(".display #time")
+    const time = document.querySelector("#time")
 
     let date = new Date()
     let hours = date.getHours()
     let minutes = date.getMinutes()
     let seconds = date.getSeconds()
 
-    let prepand = "AM"
-
-    if(hours >  12){
-        hours -= 12
-        prepand = "PM"
-    }
     if(hours < 10) {
         hours = "0"  + hours
     }
@@ -23,5 +17,5 @@ setInterval(()=>{
         seconds = "0"  + seconds
     }
     
-    time.textContent = hours + ":" + minutes + ":" + seconds + " " + prepand
+    time.textContent = hours + ":" + minutes + ":" + seconds
 });
